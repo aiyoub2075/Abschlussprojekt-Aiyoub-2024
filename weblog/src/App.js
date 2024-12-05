c34119e0fd808cb492d12a0326085dbb44ad9a9e
aiyoub2075 <aiyoub.reichert@edu.techstarter.de>
2024-12-05 19:05:39 +0100

    Routing und Navigation

--- weblog/src/App.js
+++ weblog/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import PostList from './components/PostList';
+import Home from './components/Home';
+import PostDetail from './components/PostDetail';
 import { posts } from './data/dummyData';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <Header />
-    <PostList posts={posts} />
-    <Footer />
-  </div>
+  <Router>
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/posts" exact render={() => <PostList posts={posts} />} />
+        <Route path="/posts/:id" render={() => <PostDetail posts={posts} />} />
+      </Switch>
+      <Footer />
+    </div>
+  </Router>
 );
 
 export default App;
--- weblog/src/components/Post.js
+++ weblog/src/components/Post.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CommentList from './CommentList';
 
 const Post = ({ post }) => (
   <div>
-    <h2>{post.title}</h2>
+    <h2>
+      <Link to={`/posts/${post.id}`}>{post.title}</Link>
+    </h2>
     <p>{post.content}</p>
     <CommentList comments={post.comments} />
   </div>
